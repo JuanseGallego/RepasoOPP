eed1653121b2b590e49b7a52ecfa47cf8b00086a
Matthew <matthew.corzo@estudiantesunibague.edu.co>
2026-02-06 10:03:13 -0500

    Implementacion completa de notificacion clash royal

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public class Main {
         cuenta1.consignar(150);
         System.out.println("El saldo de la cuenta es: " + cuenta1.getSaldo());
 
-        Notificacion[] notificaciones = new Notificacion[2];
+        Notificacion[] notificaciones = new Notificacion[4];
         notificaciones[0] = new NotificacionCorreo("Pepito");
         notificaciones[1] = new NotificacionSMS("Juanito");
+        notificaciones[2] = new NotificacionClashRoyal("Steven");
 
         notificaciones[0].enviarMensaje("Hola mi amigo");
         notificaciones[1].enviarMensaje("Hola mi broco");
+        notificaciones[2].enviarMensaje("¡La batalla te espera en la Arena!");
 
     }
 }
